e84a12d244e67d620f69c55ebb35bfb3835ef0d5
John Park <176120986+jprk8@users.noreply.github.com>
2025-07-15 18:20:00 -0700

    Setup Express and EJS

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('node:path');
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/indexRouter');
+const newRouter = require('./routes/newRouter');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/new', newRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`My first Express app - listening on port ${PORT}!`);
+});
